3dc34aef05a6b55d70c52a01a9d6fa38e92128e3
wcdnail@gmail.com <wcdnail@gmail.com@7779552b-9b6b-3dc8-9297-6682c7a081a0>
2014-09-01 17:43:36 +0000

    Still no any effect with i2c lcd

--- trunk/projroot/afx/dwrite.h
+++ trunk/projroot/afx/dwrite.h
@@ -4,10 +4,10 @@
 #include <avr/io.h>
 
 #define _din(port, pin)			((PIN##port & (1 << pin)) != 0)
-#define _dset(port, pin)		PORT##port |= (1 << pin)
-#define _dclr(port, pin)		PORT##port &= ~(1 << pin)
+#define _dset(port, pin)		(PORT##port |= (1 << pin))
+#define _dclr(port, pin)		(PORT##port &= ~(1 << pin))
 
-#define _dwritable(port, pin)	DDR##port |= (1 << pin)
-#define _dreadable(port, pin)	DDR##port &= ~(1 << pin)
+#define _dwritable(port, pin)	(DDR##port |= (1 << pin))
+#define _dreadable(port, pin)	(DDR##port &= ~(1 << pin))
 
 #endif /* _AFX_DWRITE_H__ */
--- trunk/projroot/lcd/lcd.main.cxx
+++ trunk/projroot/lcd/lcd.main.cxx
@@ -1,7 +1,25 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal.h>
+#include <4arduino/lcd-1.0/LiquidCrystal_I2C.h>
 #include <afx/sleep.h>
 #include <afx/dwrite.h>
 
 int main(void)
 {
+	init();
+
+	LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+	lcd.init();
+	lcd.clear();
+	lcd.setBacklight(255);
+	lcd.printstr("Trolololo...");
+
+	for (;;)
+	{
+		::sleep200ms();
+	}
+
 	return 0;
 }
