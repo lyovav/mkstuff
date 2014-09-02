61ab2887962338f1a356d82cfee6f33e47998701
wcdnail <wcdnail@gmail.com>
2014-09-02 02:16:33 +0000

    Now it WORKS! I2C lcd on atmega run at 16Mhz

--- projroot/afx/sleep.h
+++ projroot/afx/sleep.h
@@ -1,54 +1,6 @@
 #ifndef _AFX_SLEEP_H__
 #define _AFX_SLEEP_H__
 
-/***
- * sleep stuff UNIT TEST:
- *
- *
-		#include <afx/sleep.h>
-		#include <afx/dwrite.h>
-
-		// Testing diode on PD0 (1 pin of mcu)
-
-		#define _blink(t1, t2) 	\
-			do { 				\
-				_dset(D, 0); 	\
-				sleep##t1(); 	\
-				_dclr(D, 0); 	\
-				sleep##t2(); 	\
-			} while (0)
-
-
-		static void _sleep_test(void)
-		{
-			_dwritable(D, 0);
-
-			while (1)
-			{
-				_blink(2s, 2s);
-				_blink(1s, 1s);
-				_blink(500ms, 500ms);
-				_blink(400ms, 400ms);
-				_blink(300ms, 300ms);
-				_blink(200ms, 200ms);
-				_blink(100ms, 100ms);
-				_blink(50ms, 50ms);
-				_blink(40ms, 40ms);
-				_blink(30ms, 30ms);
-				_blink(20ms, 20ms);
-				_blink(10ms, 10ms);
-				_blink(5ms, 5ms);
-				_blink(4ms, 4ms);
-				_blink(3ms, 3ms);
-				_blink(2ms, 2ms);
-				_blink(1ms, 1ms);
-				sleep1s();
-			}
-		}
-
- *
- */
-
 #ifndef __ASSEMBLER__
 
 #ifdef __cplusplus
