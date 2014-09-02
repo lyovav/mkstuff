ff4fe202b3e9063968473775cf5a22fbe50a0bd5
wcdnail@gmail.com <wcdnail@gmail.com@7779552b-9b6b-3dc8-9297-6682c7a081a0>
2014-09-02 02:16:33 +0000

    Now it WORKS! I2C lcd on atmega run at 16Mhz

--- trunk/projroot/lcd/lcd.main.cxx
+++ trunk/projroot/lcd/lcd.main.cxx
@@ -5,20 +5,31 @@
 #include <afx/sleep.h>
 #include <afx/dwrite.h>
 
+extern void sleep_test(void);
+extern void sleep_test_init(void);
+
 int main(void)
 {
 	init();
 
-	LiquidCrystal_I2C lcd(0x27);
+	//LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+	//LiquidCrystal_I2C lcd(0x20, 4, 5, 6, 0, 1, 2, 3, 7, NEGATIVE);
+	LiquidCrystal_I2C lcd(0x20, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 	lcd.begin(16, 2);
+	lcd.backlight();
 	lcd.home();
-	lcd.setBacklight(255);
-	lcd.print(1024);
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print("1234567890123456");
+	lcd.setCursor(0, 1);
+	lcd.print("Line #2 ....");
+
+	sleep_test_init();
 
 	for (;;)
 	{
-		::sleep200ms();
+		sleep_test();
 	}
 
 	return 0;
